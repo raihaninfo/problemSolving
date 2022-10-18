f8e1a8e2861a153b99fc4957db48f44681cad2b4
raihaninfo <raihanmahmudi35@gmail.com>
2022-10-18 22:50:48 +0600

    solved toph  problem

--- formatedNumber/main.go
+++ formatedNumber/main.go
@@ -3,22 +3,32 @@ package main
 import (
 	"fmt"
 	"strconv"
+	"strings"
 )
 
 func main() {
 	var n int
 	fmt.Scan(&n)
+	var j int
 	s := strconv.Itoa(n)
 
-	num := []int{}
+	num := []string{}
 
 	for i := len(s) - 1; i >= 0; i-- {
-
-		//
-
-		num = append(num, int(s[i]-'0'))
+		if j == 3 {
+			num = append(num, ",")
+			num = append(num, string(s[i]))
+			j = 0
+		} else {
+			num = append(num, string(s[i]))
+		}
+		j++
 
 	}
-	fmt.Println(num)
+	newNum := []string{}
+	for i := len(num) - 1; i >= 0; i-- {
+		newNum = append(newNum, num[i])
+	}
+	fmt.Println(strings.Join(newNum, ""))
 
 }
